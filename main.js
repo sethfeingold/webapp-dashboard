19822840800bd6d4360a3c373733e5e5f8e790fc
SethFeingold <sfeingold1104@gmail.com>
2019-11-11 13:25:12 -0800

    js file and alert functionality added

---
+++ main.js
@@ -0,0 +1,16 @@
+const alert = document.getElementById("alert");
+
+alert.innerHTML =
+    `
+    <div class="alert-banner">
+        <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+        <p class="alert-banner-close">X</p>
+    </div>
+    `;
+
+alert.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+        alert.style.display = "none";
+    }
+});
