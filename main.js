c332744ace6af85a02db131e3b619e65a8c87d73
SethFeingold <sfeingold1104@gmail.com>
2019-11-11 16:46:09 -0800

    daily traffic bar chart added

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const alert = document.getElementById("alert");
 const trafficCanvas = document.getElementById("traffic-chart");
+const dailyCanvas = document.getElementById("daily-traffic-chart");
+
+// Traffic Data Line Chart
 
 let trafficData = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
@@ -31,6 +34,37 @@ let trafficChart = new Chart(trafficCanvas, {
     type: 'line',
     data: trafficData,
     options: trafficOptions
+});
+
+// Daily Traffic Bar Chart
+
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+        label: '# of Hits',
+        data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: '#7477BF',
+        borderWidth: 1
+    }]
+};
+
+const dailyOptions = {
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }]
+    },
+    legend : {
+        display: false
+    }
+}
+
+let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
 });
 
 alert.innerHTML =
