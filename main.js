58af95e843554639b9707283e30e2496327a0269
SethFeingold <sfeingold1104@gmail.com>
2019-11-11 17:00:49 -0800

    mobile data donut chart added, all charts adjusted for mobile responsiveness!

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const alert = document.getElementById("alert");
 const trafficCanvas = document.getElementById("traffic-chart");
 const dailyCanvas = document.getElementById("daily-traffic-chart");
+const mobileCanvas = document.getElementById("mobile-users-chart");
 
 // Traffic Data Line Chart
 
@@ -66,6 +67,40 @@ let dailyChart = new Chart(dailyCanvas, {
     data: dailyData,
     options: dailyOptions
 });
+
+// Mobile Users Donut Chart
+
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+        label: '# of Users',
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            '#7477BF',
+            '#78CF82',
+            '#51B6C8'
+        ]
+    }]
+};
+
+const mobileOptions = {
+    legend: {
+        position: 'right',
+        labels: {
+            boxWidth: 20,
+            fontStyle: 'bold'
+        }
+    }
+}
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
+});
+
+// Alert
 
 alert.innerHTML =
     `
